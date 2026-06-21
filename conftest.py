19227668fd0a3ca6ad2aaf5d4a15a0ffef10bd40
Anton <antonms2002@gmail.com>
2026-06-22 01:50:30 +0300

    refactor auth fixture. Add new options: screenshot and trace on failure

--- conftest.py
+++ conftest.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import BrowserContext, Browser
+from playwright.sync_api import BrowserContext, Browser, Page, Cookie
 import pytest
 import logging
 from faker import Faker
@@ -24,27 +24,39 @@ def fake():
     fake = Faker()
     return fake
 
-@pytest.fixture(scope='session')
-def context_auth(browser: Browser):
-    context = browser.new_context()
-    page = context.new_page()
-
+@pytest.fixture(scope="session")
+def auth_cookies(browser: Browser):
+    """auth_cookies fixture returns cookies of authenticated user. Login performs once in session"""
+    context: BrowserContext = browser.new_context()
+    page: Page = context.new_page()
     login_page = LoginPage(page)
     login_page.navigate(path=login_page.path)
     login_page.login(login=login_page.default_login, password=login_page.default_password)
     login_page.navbar.check_is_logout_link_displayed()
+    cookies = context.cookies()
     page.close()
-
-    yield context
     context.close()
+    return cookies
 
-@pytest.fixture()
-def page_auth(context_auth: BrowserContext):
-    page = context_auth.new_page()
-    yield page
-    page.close()
-
-
+# @pytest.fixture(scope='session')
+# def context_auth(browser: Browser):
+#     context = browser.new_context()
+#     page = context.new_page()
+#
+#     login_page = LoginPage(page)
+#     login_page.navigate(path=login_page.path)
+#     login_page.login(login=login_page.default_login, password=login_page.default_password)
+#     login_page.navbar.check_is_logout_link_displayed()
+#     page.close()
+#
+#     yield context
+#     context.close()
+#
+# @pytest.fixture()
+# def page_auth(context_auth: BrowserContext):
+#     page = context_auth.new_page()
+#     yield page
+#     page.close()
 
 # @pytest.fixture()
 # def page(context):
--- pom/tests/test_cart_page.py
+++ pom/tests/test_cart_page.py
@@ -1,12 +1,13 @@
 from pom.pages.cart_page import CartPage
-
+from playwright.sync_api import Page
 
 def test_is_cart_empty_text_present(page):
     cart_page = CartPage(page)
     cart_page.navigate(path=cart_page.path)
     cart_page.check_empty_cart_text()
 
-def test_is_cart_empty_text_present_auth_user(page_auth):
-    cart_page = CartPage(page_auth)
+def test_is_cart_empty_text_present_auth_user(page: Page, auth_cookies):
+    page.context.add_cookies(auth_cookies)
+    cart_page = CartPage(page)
     cart_page.navigate(path=cart_page.path, block_ads=False) # page_auth enable block_ads
     cart_page.check_empty_cart_text()
